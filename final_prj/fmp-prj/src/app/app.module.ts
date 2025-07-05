5ad923db815d0dbb90b9e2221cf668b621427983
vhd20000 <58044087+vhd20000@users.noreply.github.com>
2025-07-05 22:43:34 +0100

    Update app component / module

--- final_prj/fmp-prj/src/app/app.module.ts
+++ final_prj/fmp-prj/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SharedModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
