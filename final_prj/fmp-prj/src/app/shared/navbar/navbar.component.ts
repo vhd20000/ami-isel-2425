85ae8eee79a085854629e1be17c5371205744af0
vhd20000 <58044087+vhd20000@users.noreply.github.com>
2025-07-05 22:41:18 +0100

    Create Navbar component

---
+++ final_prj/fmp-prj/src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  standalone: false
+})
+export class NavbarComponent  implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  toggleDarkMode(): void {
+    document.body.classList.toggle('dark');
+  };
+
+}
