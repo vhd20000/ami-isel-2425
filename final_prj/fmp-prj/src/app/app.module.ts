b8341cc196ae8c38221a925ac291c56b11f37c5d
vhd20000 <58044087+vhd20000@users.noreply.github.com>
2025-07-06 15:41:56 +0100

    Add firebase connection to AppModule

--- final_prj/fmp-prj/src/app/app.module.ts
+++ final_prj/fmp-prj/src/app/app.module.ts
@@ -4,14 +4,36 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
+import { initializeApp } from "@firebase/app";
+import { getFirestore } from "@firebase/firestore";
+import { getAuth } from "@firebase/auth";
+import { provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore } from '@angular/fire/firestore';
+import { provideAuth } from '@angular/fire/auth';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1WTO1ubC6kP92PBGiIQUQB4RoZo_pL20",
+  authDomain: "ami-fmp-prj.firebaseapp.com",
+  projectId: "ami-fmp-prj",
+  storageBucket: "ami-fmp-prj.firebasestorage.app",
+  messagingSenderId: "180610397513",
+  appId: "1:180610397513:web:47f73b900cd6558fc8d698"
+};
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SharedModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth())
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
