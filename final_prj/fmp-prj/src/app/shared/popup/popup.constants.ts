51b4411fe54c674bae5c09a363d7006c42391c63
vhd20000 <58044087+vhd20000@users.noreply.github.com>
2025-07-08 21:47:01 +0100

    Create popup text constant file

---
+++ final_prj/fmp-prj/src/app/shared/popup/popup.constants.ts
@@ -0,0 +1,3 @@
+export const CLOSE_APP_HEADER: string = "Fechar app";
+export const CLOSE_APP_SUB_HEADER: string = "(isto é um subhear)";
+export const CLOSE_APP_SUB_MSG: string = "quer fechasr a app?";
