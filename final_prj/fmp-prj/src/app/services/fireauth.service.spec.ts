01462b1abcb33492a26c33828c39849976f9f636
vhd20000 <58044087+vhd20000@users.noreply.github.com>
2025-07-08 01:17:07 +0100

    Move FireAuthService to directory "services"

--- final_prj/fmp-prj/src/app/services/fireauth.service.spec.ts
+++ final_prj/fmp-prj/src/app/services/fireauth.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { FireauthService } from './fireauth.service';
+import { FireauthService } from './fireauth/fireauth.service';
 
 describe('FireauthService', () => {
   let service: FireauthService;
