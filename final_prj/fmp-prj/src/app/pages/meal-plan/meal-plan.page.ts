55aabc494ea554240ad4e29b94fa56b62a3d3527
vhd20000 <58044087+vhd20000@users.noreply.github.com>
2025-07-08 01:24:58 +0100

    Create MealPlan page

--- final_prj/fmp-prj/src/app/pages/meal-plan/meal-plan.page.ts
+++ final_prj/fmp-prj/src/app/pages/meal-plan/meal-plan.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NavbarComponent } from 'src/app/shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-meal-plan',
@@ -9,9 +8,51 @@ import { NavbarComponent } from 'src/app/shared/navbar/navbar.component';
 })
 export class MealPlanPage implements OnInit {
 
+  // FIXME temporary data
+  public mealPlan: any = [
+    {
+      date: new Date("7/7/2025"),
+      recepies: [
+        {
+          name: "Esparguete à bolonhesa",
+          mealType: "Almoço"
+        },
+        {
+          name: "Caldeirada de peixe",
+          mealType: "Jantar"
+        },
+      ],
+    },
+    {
+      date: new Date("7/8/2025"),
+      recepies: [
+        {
+          name: "Arroz de atum",
+          mealType: "Almoço"
+        },
+        {
+          name: "Jardineira de frango",
+          mealType: "Jantar"
+        },
+      ],
+    },
+    {
+      date: new Date("7/9/2025"),
+      recepies: [
+        {
+          name: "Lasanha de carne",
+          mealType: "Almoço"
+        },
+        {
+          name: "Caldo verde",
+          mealType: "Jantar"
+        },
+      ],
+    },
+  ];
+
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
