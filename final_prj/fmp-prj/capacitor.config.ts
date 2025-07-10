0044ccac18c1582e82863cad745bc2f21b6350e8
vhd20000 <58044087+vhd20000@users.noreply.github.com>
2025-07-10 22:58:12 +0100

    Change android project name

--- final_prj/fmp-prj/capacitor.config.ts
+++ final_prj/fmp-prj/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'fmp-prj',
+  appId: 'com.example.app1',
+  appName: 'Family Meal Planner',
   webDir: 'www'
 };
 
