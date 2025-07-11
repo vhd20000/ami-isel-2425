8a98267da299421384dc27724a3c5fa0d0bcc702
vhd20000 <58044087+vhd20000@users.noreply.github.com>
2025-07-11 23:35:26 +0100

    Update MealPlan class

--- final_prj/fmp-prj/src/app/models/meal-plan.ts
+++ final_prj/fmp-prj/src/app/models/meal-plan.ts
@@ -1,2 +1,15 @@
+import { Timestamp } from "firebase/firestore";
+import { Recepy } from "./recepy";
+
 export class MealPlan {
+    public plan!: MealPlanEntry[];
+
+    constructor(plan: MealPlanEntry[]) {
+        this.plan = plan;
+    }
+}
+
+class MealPlanEntry {
+    public date!: Timestamp;
+    public recepies!: Recepy[];
 }
