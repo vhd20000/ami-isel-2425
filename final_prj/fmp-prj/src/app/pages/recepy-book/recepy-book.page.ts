ee6cb8a101c4925f1d0157731602b9452b628151
vhd20000 <58044087+vhd20000@users.noreply.github.com>
2025-07-11 23:41:46 +0100

    Update RecepyBook page

--- final_prj/fmp-prj/src/app/pages/recepy-book/recepy-book.page.ts
+++ final_prj/fmp-prj/src/app/pages/recepy-book/recepy-book.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemReorderEventDetail } from '@ionic/angular';
+import { Recepy } from 'src/app/models/recepy';
+import { UtilityService } from 'src/app/services/utility.service';
+
+const SEARCH_RECEPY_BY_NAME_PLACEHOLDER = "Procurar por nome ...";
 
 @Component({
   selector: 'app-recepy-book',
@@ -8,9 +13,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecepyBookPage implements OnInit {
 
-  constructor() { }
+  public searchbarPlaceholder: string = SEARCH_RECEPY_BY_NAME_PLACEHOLDER;
 
-  ngOnInit() {
+  public reorderEntries: boolean = false;
+  public isSearchbarOpened: boolean = false;
+
+  public recepyBook: Recepy[] = [
+    {
+      id: "a",
+      recepyName: "Arroz de pato",
+      prepTimeMins: 40,
+      servings: "4-6",
+    } as Recepy,
+    {
+      id: "b",
+      recepyName: "Sopa",
+      prepTimeMins: 20,
+      servings: "2",
+    } as Recepy,
+  ];
+
+  public showRecepies: Recepy[] = [];
+
+  constructor(public util: UtilityService) {
+    this.showRecepies = [...this.recepyBook];
+  }
+
+  ngOnInit() { }
+
+  /**
+   * PUBLIC METHODS
+   */
+  public toggleReorderItems() {
+    this.reorderEntries = !this.reorderEntries;
+  }
+
+  public toggleSearchbar() {
+    this.isSearchbarOpened = !this.isSearchbarOpened;
+  }
+
+  public handleInput(event: Event) {
+    const target = event.target as HTMLIonSearchbarElement;
+    const query = target.value?.toLowerCase() || '';
+    if (query === '') {
+      this.showRecepies = [...this.recepyBook];
+      return;
+    }
+    this.showRecepies = this.recepyBook.filter(recepy => recepy.recepyName.toLowerCase().includes(query));
+  }
+
+  public handleReorder(event: CustomEvent<ItemReorderEventDetail>) {
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. This method can also be called directly
+    // by the reorder group
+    event.detail.complete();
   }
 
 }
