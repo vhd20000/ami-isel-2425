5bbf66d90d613cc98f28c88ca6ad70152db3a0aa
vhd20000 <58044087+vhd20000@users.noreply.github.com>
2025-07-13 00:02:33 +0100

    Update popup.constants.ts

--- final_prj/fmp-prj/src/app/shared/popup/popup.constants.ts
+++ final_prj/fmp-prj/src/app/shared/popup/popup.constants.ts
@@ -1,3 +1,5 @@
-export const CLOSE_APP_HEADER: string = "Fechar app";
+export const CLOSE_APP_HEADER: string = "Sair da aplicação";
 export const CLOSE_APP_SUB_HEADER: string = "(isto é um subhear)";
-export const CLOSE_APP_SUB_MSG: string = "quer fechasr a app?";
+export const CLOSE_APP_SUB_MSG: string = "Pretende fechar a aplicação?";
+
+export const OTHER: string = "";
