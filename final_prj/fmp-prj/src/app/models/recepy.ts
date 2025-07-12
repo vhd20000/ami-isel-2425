b0d762d6a724db8c2e6a2fc39b780dc37fd3a622
vhd20000 <58044087+vhd20000@users.noreply.github.com>
2025-07-12 23:49:59 +0100

    Update recepy.ts

--- final_prj/fmp-prj/src/app/models/recepy.ts
+++ final_prj/fmp-prj/src/app/models/recepy.ts
@@ -1,9 +1,13 @@
 export class Recepy {
     public id: string = "";
     public name: string = "";
-    public mealType: string = "";
+    public prepTimeMins: number = 0;
+    public servings: number = 0;
+    public calories?: number;
+    public categories?: string[];
+    public ingredients?: string[];
+    public steps?: string[];
     public imageId?: string;
     public image?: string;
-    public servings?: string;
-    public prepTimeMins?: number;
+    public mealType?: string;
 }
