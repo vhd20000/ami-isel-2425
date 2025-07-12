2a5861c650b08ad0d25bb0cb60cbc6d947de342e
vhd20000 <58044087+vhd20000@users.noreply.github.com>
2025-07-13 00:01:59 +0100

    Update app-routing.module.ts

--- final_prj/fmp-prj/src/app/app-routing.module.ts
+++ final_prj/fmp-prj/src/app/app-routing.module.ts
@@ -19,7 +19,11 @@ const routes: Routes = [
   {
     path: 'register',
     loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+  },  {
+    path: 'recepy-form',
+    loadChildren: () => import('./pages/recepy-form/recepy-form.module').then( m => m.RecepyFormPageModule)
   }
+
 ];
 @NgModule({
   imports: [
