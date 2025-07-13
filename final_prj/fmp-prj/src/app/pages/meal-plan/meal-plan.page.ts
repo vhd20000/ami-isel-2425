088294c492d4b0e4ed76df5d507293718f133163
vhd20000 <58044087+vhd20000@users.noreply.github.com>
2025-07-13 18:25:17 +0100

    Update MealPlan page

--- final_prj/fmp-prj/src/app/pages/meal-plan/meal-plan.page.ts
+++ final_prj/fmp-prj/src/app/pages/meal-plan/meal-plan.page.ts
@@ -1,7 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Platform } from '@ionic/angular';
 import { MealPlan } from 'src/app/models/meal-plan';
+import { Recepy } from 'src/app/models/recepy';
 import { FireService } from 'src/app/services/fire.service';
 import { UtilityService } from 'src/app/services/utility.service';
+
+const MEAL_PLAN_ROUTE_PATH: string = "/tabs/meal-plan";
+const NO_RECEPIES_REGIST_ERROR_MSG: string = "Não tem receitas registadas, por favor, acrescente algumas.";
+const UPLOAD_MEAL_PLAN_LOADING_MSG = "A gerar um novo plano ...";
+const UPLOAD_MEAL_PLAN_SUCCESS_MSG = "Plano gerado com sucesso !";
+const UPLOAD_MEAL_PLAN_FAIL_MSG = "Ocorreu um erro ao gerar um novo plano.";
+const GENERATED_MEAL_PLAN_NOTIFICATION_TITLE = "Plano atualizado";
+const GENERATED_MEAL_PLAN_NOTIFICATION_BODY = "O seu novo plano de refeições semanal está pronto! Venha ver o que preparamos para si.";
+const LOADING_PLAN_MSG: string = "A carregar plano ...";
+const DEBUG_WAIT_TIME: number = 1000;
 
 @Component({
   selector: 'app-meal-plan',
@@ -11,92 +24,73 @@ import { UtilityService } from 'src/app/services/utility.service';
 })
 export class MealPlanPage implements OnInit {
 
-  // FIXME temporary data
-  // public mealPlan: any = [
-  //   {
-  //     date: new Date("7/7/2025"),
-  //     recepies: [
-  //       {
-  //         name: "Esparguete à bolonhesa",
-  //         mealType: "Almoço",
-  //         id: "1"
-  //       },
-  //       {
-  //         name: "Caldeirada de peixe",
-  //         mealType: "Jantar",
-  //         id: "2"
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     date: new Date("7/8/2025"),
-  //     recepies: [
-  //       {
-  //         name: "Arroz de atum",
-  //         mealType: "Almoço",
-  //         id: "3"
-  //       },
-  //       {
-  //         name: "Jardineira de frango",
-  //         mealType: "Jantar",
-  //         id: "4"
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     date: new Date("7/9/2025"),
-  //     recepies: [
-  //       {
-  //         name: "Lasanha de carne",
-  //         mealType: "Almoço"
-  //       },
-  //       {
-  //         name: "Caldo verde",
-  //         mealType: "Jantar"
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     date: new Date("7/10/2025"),
-  //     recepies: [
-  //       {
-  //         name: "Lasanha de carne",
-  //         mealType: "Almoço"
-  //       },
-  //       {
-  //         name: "Caldo verde",
-  //         mealType: "Jantar"
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     date: new Date("7/11/2025"),
-  //     recepies: [
-  //       {
-  //         name: "Lasanha de carne",
-  //         mealType: "Almoço"
-  //       },
-  //       {
-  //         name: "Caldo verde",
-  //         mealType: "Jantar"
-  //       },
-  //     ],
-  //   },
-  // ];
-
-  public mealPlan: MealPlan = new MealPlan([]);
+  public mealPlan: MealPlan | null = null;
+  public firstLoad: boolean = true;
 
   constructor(
     private fireService: FireService,
-    public util: UtilityService,
+    private router: Router,
+    public util: UtilityService
   ) {
-    fireService.getUserMealPlan().then(mealPlan => {
-      console.log(mealPlan);
-      if (!mealPlan) return;
-      this.mealPlan = mealPlan;
+    // Load recepy list from Firebase to populate page
+    this.router.events.subscribe(e => {
+      if (this.firstLoad || (e instanceof NavigationEnd && e.url === MEAL_PLAN_ROUTE_PATH && this.firstLoad)) {
+        this.firstLoad = false;
+        const loading = this.util.createLoading();
+        loading.present(LOADING_PLAN_MSG);
+        fireService.getUserMealPlan().then(mealPlan => {
+          loading.dismiss();
+          if (!mealPlan) return;
+          this.mealPlan = mealPlan;
+        });
+      }
     });
   }
 
   ngOnInit() { }
+
+  /**
+   * PUBLIC METHODS
+   */
+
+  
+  public async tryUpdateMealPlan() {
+    const userRecepies: Recepy[] = await this.fireService.getUserRecepyBook() ?? [];
+
+    if (userRecepies.length < 1) {
+      this.util.openToast(NO_RECEPIES_REGIST_ERROR_MSG);
+      return;
+    }
+
+    // Generate a new plan (pick random meals from user's recepy book)
+    const newMealPlan: MealPlan = this.util.generateNewMealPlan(userRecepies);
+
+    // Try upload new plan to firestore
+    let showSuccessNotification: boolean = false;
+    const loading = this.util.createLoading();
+    loading.present(UPLOAD_MEAL_PLAN_LOADING_MSG);
+    // [DEBUG] Set a timer to mimic a wait in db response
+    await this.util.delay(DEBUG_WAIT_TIME);
+    await this.fireService.updateUserMealPlan(newMealPlan)
+      .then(res => {
+        this.util.openToast(UPLOAD_MEAL_PLAN_SUCCESS_MSG);
+        showSuccessNotification = true;
+      })
+      .catch(err => {
+        this.util.openToast(UPLOAD_MEAL_PLAN_FAIL_MSG);
+      })
+      .finally(() => {
+        loading.dismiss();
+      });
+
+      // redirect on success
+      if (showSuccessNotification) {
+        this.mealPlan = newMealPlan;
+        this.util.schduleBasicLocalNotification(
+          GENERATED_MEAL_PLAN_NOTIFICATION_TITLE,
+          GENERATED_MEAL_PLAN_NOTIFICATION_BODY
+        );
+      }
+  }
 
 }
