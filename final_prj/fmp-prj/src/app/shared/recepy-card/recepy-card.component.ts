6209fc50c4e0544b9d8be127a86898dc42140faf
vhd20000 <58044087+vhd20000@users.noreply.github.com>
2025-07-13 18:26:04 +0100

    Update RecepyCard component

--- final_prj/fmp-prj/src/app/shared/recepy-card/recepy-card.component.ts
+++ final_prj/fmp-prj/src/app/shared/recepy-card/recepy-card.component.ts
@@ -1,9 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ItemReorderEventDetail } from '@ionic/angular';
 import { Recepy } from 'src/app/models/recepy';
+import { FireService } from 'src/app/services/fire.service';
 import { UtilityService } from 'src/app/services/utility.service';
 
 const RECEPY_PIC: string = "https://ionicframework.com/docs/img/demos/thumbnail.svg";
+const DELETE_RECEPY_LOADING_MSG = "A apagar receita ...";
+const DELETE_RECEPY_SUCCESS_MSG = "Receita apagada com sucesso !";
+const DELETE_RECEPY_ERROR_MSG: string = "Ocorreu um erro ao apagar uma receita.";
 
 @Component({
   selector: 'app-recepy-card',
@@ -15,10 +19,14 @@ export class RecepyCardComponent  implements OnInit {
 
   @Input() recepy!: Recepy;
   @Input() isReorderModeActive: boolean = false;
+  @Output() deleteElement: EventEmitter<any> = new EventEmitter();
 
   public recepyPic: string = RECEPY_PIC;
 
-  constructor(public util: UtilityService) { }
+  constructor(
+    public fireService: FireService,
+    public util: UtilityService,
+  ) { }
 
   ngOnInit() {}
 
@@ -26,10 +34,32 @@ export class RecepyCardComponent  implements OnInit {
    * PUBLIC METHODS
    */
 
-  public deleteRecepyByIdHandler(recepyId: string) {
+  public editRecepyByIdHandler(recepyId: string) {
     // TODO
-    this.util.openToast("Por implementar ...");
+    this.util.openToast("editar elemento ...");
     // this.deleteRecepyById();
   }
 
+  public async tryDeleteRecepyById(recepyId: string) {
+    let deleteCard: boolean = false;
+
+    // Try delete recepy from firestore
+    const loading = this.util.createLoading();
+    loading.present(DELETE_RECEPY_LOADING_MSG);
+    await this.fireService.deleteRecepyById(recepyId)
+      .then(res => {
+        deleteCard = true;
+        this.util.openToast(DELETE_RECEPY_SUCCESS_MSG);
+      })
+      .catch(err => {
+        this.util.openToast(DELETE_RECEPY_ERROR_MSG);
+      })
+      .finally(() => {
+        loading.dismiss();
+      });
+    
+    if (deleteCard) {
+      this.deleteElement.emit();
+    }
+  }
 }
