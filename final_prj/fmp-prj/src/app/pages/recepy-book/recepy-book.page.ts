96133f77e80e357fc081d905c5d101a3b150ce71
vhd20000 <58044087+vhd20000@users.noreply.github.com>
2025-07-13 18:25:30 +0100

    Update RecepyBook page

--- final_prj/fmp-prj/src/app/pages/recepy-book/recepy-book.page.ts
+++ final_prj/fmp-prj/src/app/pages/recepy-book/recepy-book.page.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { ItemReorderEventDetail } from '@ionic/angular';
 import { Recepy } from 'src/app/models/recepy';
 import { FireService } from 'src/app/services/fire.service';
 import { UtilityService } from 'src/app/services/utility.service';
 
-const ADD_RECEPY_FORM_PAGE = "recepy-form";
+const RECEPY_BOOK_ROUTE_PATH: string = "/tabs/recepy-book";
+const ADD_RECEPY_FORM_ROUTE_PATH = "recepy-form";
 const SEARCH_RECEPY_BY_NAME_PLACEHOLDER = "Procurar por nome ...";
+const LOADING_RECEPY_BOOK_MSG: string = "A carregar receitas ...";
 
 @Component({
   selector: 'app-recepy-book',
@@ -15,7 +18,7 @@ const SEARCH_RECEPY_BY_NAME_PLACEHOLDER = "Procurar por nome ...";
 })
 export class RecepyBookPage implements OnInit {
 
-  public addRecepyFormPage: string = ADD_RECEPY_FORM_PAGE;
+  public addRecepyFormRoutePath: string = ADD_RECEPY_FORM_ROUTE_PATH;
   public searchbarPlaceholder: string = SEARCH_RECEPY_BY_NAME_PLACEHOLDER;
 
   public reorderEntries: boolean = false;
@@ -23,17 +26,29 @@ export class RecepyBookPage implements OnInit {
   public recepyBook: Recepy[] | null = null;
   public showRecepies: Recepy[] = [];
 
+  public static firstLoad: boolean = true;
+
   constructor(
-      private fireService: FireService,
-      public util: UtilityService,
-    ) {
-      fireService.getUserRecepyBook().then(recepyBook => {
-        console.log(recepyBook);
-        if (!recepyBook) return;
-        this.recepyBook = recepyBook;
-        this.showRecepies = recepyBook;
-      });
-    }
+    private fireService: FireService,
+    public util: UtilityService,
+    private router: Router
+  ) {
+    // Load recepy list from Firebase to populate page
+    this.router.events.subscribe(e => {
+      if (RecepyBookPage.firstLoad || (e instanceof NavigationEnd && e.url === RECEPY_BOOK_ROUTE_PATH && RecepyBookPage.firstLoad)) {
+        RecepyBookPage.firstLoad = false;
+        const loading = this.util.createLoading();
+        loading.present(LOADING_RECEPY_BOOK_MSG);
+        this.fireService.getUserRecepyBook()
+          .then(recepyBook => {
+            loading.dismiss();
+            if (!recepyBook) return;
+            this.recepyBook = recepyBook;
+            this.showRecepies = recepyBook;
+          });
+      }
+    });
+  }
 
   ngOnInit() { }
 
@@ -58,8 +73,11 @@ export class RecepyBookPage implements OnInit {
     this.showRecepies = this.recepyBook!.filter(recepy => recepy.name.toLowerCase().includes(query));
   }
 
-  public log(value: any) {
-    console.log(value);
+  public deleteRecepyLocally(recepy: Recepy) {
+    const idx: number = this.recepyBook?.indexOf(recepy) ?? -1;
+    if (idx > -1) {
+      this.recepyBook?.splice(idx, 1);
+    }
   }
 
 }
