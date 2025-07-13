3b2a657b0bd5763b2d66d0f2336ccd6f193985b3
vhd20000 <58044087+vhd20000@users.noreply.github.com>
2025-07-13 18:25:40 +0100

    Update recepy-form.page.ts

--- final_prj/fmp-prj/src/app/pages/recepy-form/recepy-form.page.ts
+++ final_prj/fmp-prj/src/app/pages/recepy-form/recepy-form.page.ts
@@ -1,14 +1,25 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoadingController } from '@ionic/angular';
 import { RecepyImage } from 'src/app/models/recepy';
 import { FireService } from 'src/app/services/fire.service';
 import { UtilityService } from 'src/app/services/utility.service';
+import { LoadingComponent } from 'src/app/shared/loading/loading.component';
+import { RecepyBookPage } from '../recepy-book/recepy-book.page';
 
 const NO_INPUT_CATEGORY_ERROR_MSG: string = "Por favor insira uma categoria.";
 const ADD_CATEGORY_ERROR_MSG: string = "Esta categoria já existe. Por favor, tente outra.";
 const REQUIRED_FIELD_MSG: string = "Campo obrigatório";
 const ADD_CATEGORY_ERROR_TOAST_DURATION: number = 3000;
 const MIN_PREP_TIME_VALUE: number = 1;
+const UPLOAD_RECEPY_LOADING_MSG = "A registar receita ...";
+const UPLOAD_RECEPY_SUCCESS_MSG = "Receita criada com sucesso !";
+const UPLOAD_RECEPY_FAIL_MSG = "Ocorreu um erro ao criar a receita.";
+const RECEPY_BOOK_PAGE_ROUTE = "/tabs/recepy-book"
+
+// FIXME
+const ADD_RECEPY_SUCCESS_NOTIFICATION_TITLE_MSG = "";
+const ADD_RECEPY_SUCCESS_NOTIFICATION_BODY_MSG = "";
 
 @Component({
   selector: 'app-recepy-form',
@@ -105,28 +116,43 @@ export class RecepyFormPage implements OnInit {
   /**
    * PULBIC METHODS
    */
-  public async tryAddRecepy(value: any) {
-    // this.util.openToast("A implementar ...", { position: ToastPosition.TOP });
-    value.name = this.util.sanitizeString(value.name);
-    value.categories = this.categories;
-    value.ingredients = this.ingrdients;
-    value.steps = this.steps;
+  public async tryAddRecepy(recepyForm: any) {
+    // Prepare data in form
+    recepyForm.name = this.util.sanitizeString(recepyForm.name);
+    recepyForm.categories = this.categories;
+    recepyForm.ingredients = this.ingrdients;
+    recepyForm.steps = this.steps;
 
-    // try upload image to firebase / wait for it to finish
+    // Try upload image to firebase / wait for it to finish
     if (this.image) {
       const blob = this.image.blob;
       const imageUid = await this.fireService.uploadImage(blob);
       const url = await this.fireService.getImageURL(imageUid);
-      value.imageURL = url;
+      recepyForm.imageURL = url;
     }
     
-    // try upload form data to firebase
-    // ...
-    console.log(value);
+    // Try upload form data to firestore
+    let returnToRecepyBookPage: boolean = false;
+    const loading = this.util.createLoading();
+    loading.present(UPLOAD_RECEPY_LOADING_MSG);
+    await this.fireService.createRecepy(recepyForm)
+      .then(res => {
+        this.util.openToast(UPLOAD_RECEPY_SUCCESS_MSG);
+        returnToRecepyBookPage = true;
+      })
+      .catch(err => {
+        this.util.openToast(UPLOAD_RECEPY_FAIL_MSG);
+      })
+      .finally(() => {
+        loading.dismiss();
+      });
 
-    // redirect on success
-    // ...
-  }
+      // redirect on success
+      if (returnToRecepyBookPage) {
+        RecepyBookPage.firstLoad = true;
+        this.util.redirectTo(RECEPY_BOOK_PAGE_ROUTE);
+      }
+    }
 
   public addCategory() {
     const newCategory: string = this.util.sanitizeString(this.categoryToAdd.nativeElement.value);
