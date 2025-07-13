3e501bdffb9370bfc8dc8852b9d10590113946ca
vhd20000 <58044087+vhd20000@users.noreply.github.com>
2025-07-13 18:23:47 +0100

    Update App component

--- final_prj/fmp-prj/src/app/app.component.ts
+++ final_prj/fmp-prj/src/app/app.component.ts
@@ -1,11 +1,5 @@
-import { Component, OnInit, Optional, ViewChild } from '@angular/core';
-import { App } from '@capacitor/app';
-import { IonRouterOutlet, Platform } from '@ionic/angular';
-import { PopupComponent } from './shared/popup/popup.component';
-import { CLOSE_APP_HEADER, CLOSE_APP_SUB_HEADER, CLOSE_APP_SUB_MSG } from './shared/popup/popup.constants';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UtilityService } from './services/utility.service';
-
-const BACK_BUTTON_EVENT_PRIORITY: number = -1;
 
 @Component({
   selector: 'app-root',
@@ -13,42 +7,14 @@ const BACK_BUTTON_EVENT_PRIORITY: number = -1;
   styleUrls: ['app.component.scss'],
   standalone: false,
 })
-export class AppComponent {
-
-  @ViewChild(PopupComponent) popupComponent!: PopupComponent;
-
-    public popHeader = CLOSE_APP_HEADER;
-    public popSubHeader = CLOSE_APP_SUB_HEADER;
-    public popMsg = CLOSE_APP_SUB_MSG;
+export class AppComponent implements OnInit {
 
   constructor(
-    private util: UtilityService,
-    private platform: Platform,
-    @Optional() private routerOutlet?: IonRouterOutlet
-  ) {
-    // This line defines the Platform back button behaviour
-    this.platform.backButton.subscribeWithPriority(BACK_BUTTON_EVENT_PRIORITY, () => {
-      this.backButtonHandler();
-    });
+    private util: UtilityService
+  ) { }
+
+  async ngOnInit() {
+    await this.util.requestLocalNotificationPermission();
   }
 
-  /**
-   * PRIVATE METHODS
-   */
-
-  private backButtonHandler() {
-    let canGoBack: boolean = this.routerOutlet?.canGoBack() ?? false;
-    this.util.openToast(`can go back ? ${canGoBack.toString()}`);
-    if (!canGoBack) {
-      this.popupComponent.openPopup();
-    }
-  }
-
-  /**
-   * PUBLIC METHODS
-   */
-
-  public exitApp() {
-    App.exitApp();
-  }
 }
