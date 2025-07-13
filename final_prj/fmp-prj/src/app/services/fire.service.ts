be072b551f846d7ede3824b90a1ae40582fc71ff
vhd20000 <58044087+vhd20000@users.noreply.github.com>
2025-07-13 18:24:27 +0100

    Update fire.service.ts

--- final_prj/fmp-prj/src/app/services/fire.service.ts
+++ final_prj/fmp-prj/src/app/services/fire.service.ts
@@ -2,20 +2,20 @@ import { Injectable, OnInit } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
 import { Storage } from '@angular/fire/storage';
 import { collectionData, Firestore } from '@angular/fire/firestore';
-import { addDoc, collection, deleteDoc, doc, getDoc, updateDoc } from 'firebase/firestore';
+import { addDoc, collection, deleteDoc, deleteField, doc, getDoc, setDoc, updateDoc } from 'firebase/firestore';
 import { Observable } from 'rxjs';
 import { UtilityService } from './utility.service';
 import { MealPlan } from '../models/meal-plan';
 import { Recepy } from '../models/recepy';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { ExceptionCode } from '@capacitor/core';
 
 /**
  * Firestore Documents
  */
 const MEAL_PLAN_DOC = 'meal-plan';
 const RECEPY_BOOK_DOC = 'recepy-book';
-const PLAN_DOC = 'plan';
-const RECEPIES_DOC = 'recepies';
+const PLAN_DOC_FIELD = 'plan';
 
 /**
  * Firebase Storage References
@@ -44,10 +44,26 @@ export class FireService {
 
     if (!docSnap.exists()) return null;
 
-    const data = docSnap.data()[PLAN_DOC];
+    const data = docSnap.data()[PLAN_DOC_FIELD];
     const mealPlan = new MealPlan(data);
 
     return mealPlan;
+  }
+
+  async updateUserMealPlan(newMealPlan: MealPlan) {
+    try {
+      const uid: string = this.afAuth.currentUser?.uid ?? "";
+      const docRef = doc(this.firestore, MEAL_PLAN_DOC, uid);
+      
+      const data: any = { plan: newMealPlan.plan };
+      await setDoc(docRef, data);
+  
+      return true;
+    }
+    catch (e) {
+      console.error("Error updating user plan: ", e);
+      return false;
+    }
   }
 
   async getUserRecepyBook() {
@@ -56,9 +72,65 @@ export class FireService {
     const docSnap = await getDoc(docRef);
 
     if (!docSnap.exists()) return null;
+    const data = docSnap.data();
 
-    const recepies = docSnap.data()[RECEPIES_DOC] as Recepy[];
+    // Extract recepies from retrieved data
+    let recepies: Recepy[] = [];
+    Object.keys(data).forEach(key => {
+      let obj = {id: key, ...data[key]} as Recepy;
+      recepies.push(obj);
+    });
+    
+    recepies.sort((a: Recepy, b: Recepy) => a.name.localeCompare(b.name));
+
     return recepies;
+  }
+
+  async createRecepy(recepy: Recepy) {
+    try {
+      const uid: string = this.afAuth.currentUser?.uid ?? "";
+      if (uid === '') throw new Error("No user");
+
+      const docRef = doc(this.firestore, RECEPY_BOOK_DOC, uid);
+      const docSnap = await getDoc(docRef);
+
+      // Prepare data to be registered
+      const recepyId = this.util.generateUniqueId();
+      let data: any = {};
+      data[recepyId] = {...recepy};
+
+      if (docSnap.exists()) {
+        await setDoc(docRef, data, { merge: true });
+      }
+      else {
+        await setDoc(docRef, data);
+      }
+      return true;
+    }
+    catch (e) {
+      console.error("Error adding document: ", e);
+      return null;
+    }
+  }
+
+  async deleteRecepyById(recepyId: string) {
+    try {
+      const uid: string = this.afAuth.currentUser?.uid ?? "";
+      if (uid === '') throw new Error("No user");
+
+      const docRef = doc(this.firestore, RECEPY_BOOK_DOC, uid);
+
+      // Prepare data to delete entry/field
+      let data: any = {};
+      data[recepyId] = deleteField();
+      
+      await updateDoc(docRef, data);
+      return true; 
+    }
+    catch (e) {
+      console.error("Error deleting document: ", e);
+      return false;
+    }
   }
 
   /**
